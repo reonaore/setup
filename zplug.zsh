2dc082f5e08754394ce9a1bea06978aa18a15d01
Leona Shiode <reona.ookikunaru@gmail.com>
2024-01-07 10:59:07 +0900

    feat: add zplug

--- zplug.zsh
+++ zplug.zsh
@@ -1,19 +1,21 @@
 export ZPLUG_HOME=$HOME/.zplug
 source $ZPLUG_HOME/init.zsh
 
+source $HOME/.zsh/plugins/bindkey.zsh
+
 zplug "~/.zsh/plugins", from:local
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-syntax-highlighting"
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zaw"
-zplug "willghatch/zsh-hooks"
-zplug "willghatch/zsh-cdr"
 zplug "git/git", from:github, use:"contrib/completion/git-prompt.sh"
 zplug "plugins/common-aliases", from:oh-my-zsh
 zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/tmux", from:oh-my-zsh
-zplug "plugins/colored-man-pages", from:oh-my-zsh
+zplug "plugins/docker", from:oh-my-zsh
+zplug "plugins/docker-compose", from:oh-my-zsh
+zplug "jeffreytse/zsh-vi-mode"
 
 if ! zplug check; then
     zplug install
